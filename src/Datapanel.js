9b059bb267c7465aab6676d4a323b9ab7e61a9d4
Josh Barnett <47127092+Urmatemugly@users.noreply.github.com>
2020-10-21 15:34:30 +0800

    state updates, events fire - add API call

--- src/Datapanel.js
+++ src/Datapanel.js
@@ -1,54 +1,36 @@
 import React, { Component } from 'react';
 import Button from './Button.js';
 
-/** Build a stateful component to supply data to its children **/
+
 class Datapanel extends Component {
+
+  /** Rover names as properties (iterate for buttons) **/
   static defaultProps = {
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
   };
+
+
   constructor(props){
     super(props);
-    /** Rover names as properties (iterate for buttons) **/
 
     /** Define elements of state  **/
     this.state = {
       chosen: false,
-      selected: {
-        name: '',
-        launch:'',
-        land:'',
-        status:'',
-        imgs:[],
-        dates:[],
-      }
+      name: '',
+      launch:'',
+      land:'',
+      status:'',
+      imgs:[],
+      dates:[],
     };
     /** Bind (this) **/
     this.getRoverName = this.getRoverName.bind(this);
   }
 
-  updateState(state) {
-    this.setState(st => ({
-      chosen: true
-    }));
+  /** Update state when button is Clicked**/
+  getRoverName(name){
+    this.setState({ chosen: true, name: (name)});
   }
-// addCount(){
-// console.log('clicked!');
-// }
-getRoverName(name){
-  console.log('clicked!');
-  console.log(name);
-}
-  /** Function to handle click**/
-  // handleClick(e) {
-  //   this.setState(state => ({
-  //     ...state,
-  //     chosen: true,
-  //     selected: {
-  //       name: event.currentTarget
-  //       }
-  //     }
-  //   ))
-  // }
 
 
   /** Render clickable buttons, displaying currently clicked**/
@@ -56,10 +38,19 @@ getRoverName(name){
     return (
       <div className="App">
       <h1>Mars Rover Dashboard</h1>
-      <h2>{this.state.selected.name}</h2>
+      {this.state.chosen ?
+        <div>
+        <h2>{this.state.name}</h2>
+          <p>Launch Date: {this.state.launch}</p>
+          <p>Landing Date: {this.state.land} </p>
+          <p>Status: {this.state.status}</p>
+          </div>
+        :
+      <h2>Click a button to get started!</h2>
+    }
       {this.props.rovers.map(rover =>
         <Button getData={this.getRoverName} name={rover}/>
-       )}
+      )}
       </div>
     )
   }
